d71b24eba2ef3452349e2dac80badba622e55689
Your Name <you@example.com>
2018-01-09 15:32:41 +0300

    Initial Commit

---
+++ _dream team/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  _dream team
+//
+//  Created by Константин Клинов on 1/9/18.
+//  Copyright © 2018 Константин Клинов. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

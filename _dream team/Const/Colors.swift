3478997294a7195cb7d8c44c12454eb1b8322bed
Your Name <you@example.com>
2018-01-10 19:35:09 +0300

    Added AuthVC

---
+++ _dream team/Const/Colors.swift
@@ -0,0 +1,11 @@
+//
+//  Colors.swift
+//  _dream team
+//
+//  Created by Константин Клинов on 1/10/18.
+//  Copyright © 2018 Константин Клинов. All rights reserved.
+//
+
+import Foundation
+
+var backgroundColor = "58BA9F"
